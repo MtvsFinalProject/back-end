29a0ce23a861b94a9cdfe47bde6d1ad9f286e47b
pyoya1123 <pyoya1123@gmail.com>
2024-11-27 23:15:26 +0900

    Feat: 유저 맵 방문 횟수 체크 기능 구현

---
+++ src/main/java/com/project/final_project/userposvisitcount/repository/UserPosVisitCountRepository.java
@@ -0,0 +1,18 @@
+package com.project.final_project.userposvisitcount.repository;
+
+import com.project.final_project.userposvisitcount.domain.UserPosVisitCount;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
+public interface UserPosVisitCountRepository extends JpaRepository<UserPosVisitCount, Integer> {
+
+  @Query("select u from UserPosVisitCount u where u.mapType = :mapType and u.userId = :userId")
+  UserPosVisitCount getUserPosVisitCountByMapIdAndMapTypeAndUserId(
+      @Param("mapType") String mapType,
+      @Param("userId") int userId);
+}
